4f7a56fe9d42a95edeb0014df3ead62298eb00d7
Blake Macnair <bmacnair@activecampaign.com>
2018-12-21 10:57:17 -0600

    Initial Commit

---
+++ DeckAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DeckAnimation
+//
+//  Created by Blake Macnair on 12/21/18.
+//  Copyright © 2018 Macnair. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
