16d46a44950b7f573302be7e3693b27f7ce04b4f
Blake Macnair <bmacnair@activecampaign.com>
2018-12-21 12:00:42 -0600

    Add class for making custom transition.

---
+++ DeckAnimation/CardAnimator.swift
@@ -0,0 +1,21 @@
+//
+//  CardAnimator.swift
+//  DeckAnimation
+//
+//  Created by Blake Macnair on 12/21/18.
+//  Copyright © 2018 Macnair. All rights reserved.
+//
+
+import UIKit
+
+public class CardAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+    public func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.3
+    }
+
+    public func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        
+    }
+
+
+}
