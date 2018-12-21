32615e520554977a7450a4782817e96d70bc94b6
Blake Macnair <bmacnair@activecampaign.com>
2018-12-21 11:06:29 -0600

    Set up project to load root VC programmatically.

--- DeckAnimation/ViewController.swift
+++ DeckAnimation/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var button: UIButton = {
+        let button =  UIButton(type: .roundedRect)
+        button.setTitle("Press me!", for: .normal)
+        button.backgroundColor = UIColor.cyan
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        view.backgroundColor = .white
+
+        view.addSubview(button)
+
+        NSLayoutConstraint.activate([
+            button.heightAnchor.constraint(equalToConstant: 80),
+            button.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
+
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            ])
     }
 
 
